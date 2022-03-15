d338c3bc6658835068a3342953a9d641b6cdec6b
Robbi3005 <97607694+Robbi3005@users.noreply.github.com>
2022-03-15 15:46:45 +0700

    tambah tutorial

---
+++ tutor-git.js
@@ -0,0 +1,10 @@
+// cara git di vs Code
+
+// git init
+// git remote add origin https://github.com/Robbi3005/Day-27.git (nama reponya)
+// git branch -M main
+
+// kalo ada file
+// di git add.
+// git commit -m "isi"
+// git push -u origin main
